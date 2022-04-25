7bb86b6fdaa6722b482ace1e448266914bf86844
nikita <nickonblack@gmail.com>
2022-04-25 19:11:24 +0300

    Добавил проверку на постановку точки, начал реализацию калькулятора

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ButtonData } from './classes/button_data';
 import { getFlex } from './utils/help_funcs';
-import { Calculator, Actions, ResultType } from './calculator/calculalator';
+import { Calculator, Actions, ResultType, SpecialSign, Operation } from './calculator/calculalator';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +11,10 @@ import { Calculator, Actions, ResultType } from './calculator/calculalator';
 export class AppComponent {
 
   calculatorButtons: ButtonData[][] = [
-    [new ButtonData('7', getFlex(1)), new ButtonData('8', getFlex(1)), new ButtonData('9', getFlex(1)), new ButtonData('÷', getFlex(1)), new ButtonData(Actions.clear, '2 0 calc(90px)')],
-    [new ButtonData('4', getFlex(1)), new ButtonData('5', getFlex(1)), new ButtonData('6', getFlex(1)), new ButtonData('x', getFlex(1)), new ButtonData('(', getFlex(1)), new ButtonData(')', getFlex(1))],
-    [new ButtonData('1', getFlex(1)), new ButtonData('2', getFlex(1)), new ButtonData('3', getFlex(1)), new ButtonData('−', getFlex(1)), new ButtonData('π', getFlex(1)), new ButtonData('e', getFlex(1))],
-    [new ButtonData('0', getFlex(1)), new ButtonData(Actions.fraction, getFlex(1)), new ButtonData(Actions.percent, getFlex(1)), new ButtonData('+', getFlex(1)), new ButtonData(Actions.compute, '2 0 calc(90px)')],
+    [new ButtonData('7', getFlex(1)), new ButtonData('8', getFlex(1)), new ButtonData('9', getFlex(1)), new ButtonData(Operation.division, getFlex(1)), new ButtonData(Actions.clear, '2 0 calc(90px)')],
+    [new ButtonData('4', getFlex(1)), new ButtonData('5', getFlex(1)), new ButtonData('6', getFlex(1)), new ButtonData(Operation.multiply, getFlex(1)), new ButtonData(Operation.leftBrace, getFlex(1)), new ButtonData(Operation.rightBrace, getFlex(1))],
+    [new ButtonData('1', getFlex(1)), new ButtonData('2', getFlex(1)), new ButtonData('3', getFlex(1)), new ButtonData(Operation.subtraction, getFlex(1)), new ButtonData(SpecialSign.pi, getFlex(1)), new ButtonData(SpecialSign.e, getFlex(1))],
+    [new ButtonData('0', '2 0 calc(90px)'), new ButtonData(Actions.fraction, getFlex(1)), new ButtonData('+', getFlex(1)), new ButtonData(Actions.compute, '2 0 calc(90px)')],
   ];
 
   calculator:Calculator = new Calculator();
--- src/app/calculator/calculalator.ts
+++ src/app/calculator/calculalator.ts
@@ -1,20 +1,50 @@
+import { BinaryTree } from '../classes/tree/tree_node';
+
 export class Calculator {
 
     result: string = ResultType.default;
 
     compute(action:string) {
         switch (action) {
-            case Actions.percent:
-                this.result = ResultType.error;
-                break;
             case Actions.clear:
                 this.result = ResultType.default;
                 break;
+            case Actions.fraction:
+                // если в конце строки есть 
+                if (this.result.endsWith(",")) {
+                    return;
+                }
+
+                // для нахождения чисел необходимо с помощью regex необходимы . вместо ,
+                let newText = this.result.replace(/,/g, '.');
+
+                const regex = /[+-]?\d+(\.\d+)?/g;
+                
+                let floats = newText.match(regex)?.map(item => parseFloat(item));
+
+                if ((floats !== undefined) && ((floats?.length ?? []) > 0)) {
+                    const lastIndex = floats!.length;
+                    const lastNumber = floats![lastIndex - 1];
+
+                    // если последнее число уже float, то запятая нек нужна  
+                    if (!Number.isInteger(lastNumber)) {
+                        return;
+                    }
+                }
+                
+                this.result += action;
+                break;
             case Actions.compute:
+                console.log("Start Computing");
+                
+                const binaryTree = new BinaryTree();
+                binaryTree.insert(5).insert(4.44).insert("fsf");
+
+                console.log(this.calculate(this.result));
                 this.result = ResultType.error;
                 break;
             default:
-                if (this.result == ResultType.default) {
+                if ((<any>Object).values(ResultType).includes(this.result)) {
                     this.result = action;
                 } else {
                     this.result += action;
@@ -22,6 +52,20 @@ export class Calculator {
                 break;
         }
     };
+    
+    calculate(fn:string) {
+        return new Function('return ' + fn)();
+    } 
+
+    private toNumber(value: string): number | null {
+      let resStr = this.replaceAllMathSigns(value);
+      console.log(resStr);
+      return 1;
+    }
+
+    private replaceAllMathSigns(value: string): string {
+        return value.replace(SpecialSign.pi, Math.PI.toString()).replace(SpecialSign.e, Math.E.toString());
+    }
 }
 
 export enum ResultType {
@@ -32,7 +76,55 @@ export enum ResultType {
 export enum Actions {
     clear = "C",
     compute = "=",
-    percent = "%",
     fraction = ",",
 }
 
+export enum SpecialSign {
+    pi = 'π',
+    e = 'e',
+}
+
+export enum Operation {
+    multiply = "x",
+    division = "÷",
+    leftBrace = "(",
+    rightBrace = ")",
+    addition = "+",
+    subtraction = "-",
+    notAnOperation = "N",
+}
+
+class Expression {
+    value: number = 0;
+    op: Operation = Operation.notAnOperation;
+    left: Expression | undefined
+    right: Expression | undefined
+
+    calculate = (): number => {
+        if (this.op == Operation.notAnOperation) {
+            return this.value;
+        }
+
+        // assert(this.left);
+        // assert(this.right);
+        let leftValue = this.left?.calculate();
+        let rightValue = this.right?.calculate();
+        
+        switch (this.op) {
+            case Operation.addition:
+                this.value = this.left!.value + this.right!.value;
+                break;
+            case Operation.subtraction:
+                this.value = this.left!.value - this.right!.value;
+                break;
+            case Operation.multiply:
+                this.value = this.left!.value * this.right!.value;
+                break;
+            case Operation.division:
+                this.value = this.left!.value / this.right!.value;
+                break;
+        }
+
+        return this.value;
+    } 
+}
