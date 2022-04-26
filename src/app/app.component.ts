0f1bf19abac829e0e8ca3602072e559a67685ccf
nikita <nickonblack@gmail.com>
2022-04-26 16:07:47 +0300

    первая стабильная версия (работет +-/)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
 
   calculatorButtons: ButtonData[][] = [
     [new ButtonData('7', getFlex(1)), new ButtonData('8', getFlex(1)), new ButtonData('9', getFlex(1)), new ButtonData(Operation.division, getFlex(1)), new ButtonData(Actions.clear, '2 0 calc(90px)')],
-    [new ButtonData('4', getFlex(1)), new ButtonData('5', getFlex(1)), new ButtonData('6', getFlex(1)), new ButtonData(Operation.multiply, getFlex(1)), new ButtonData(Operation.leftBrace, getFlex(1)), new ButtonData(Operation.rightBrace, getFlex(1))],
+    [new ButtonData('4', getFlex(1)), new ButtonData('5', getFlex(1)), new ButtonData('6', getFlex(1)), new ButtonData(Operation.multiplication, getFlex(1)), new ButtonData(Operation.leftBrace, getFlex(1)), new ButtonData(Operation.rightBrace, getFlex(1))],
     [new ButtonData('1', getFlex(1)), new ButtonData('2', getFlex(1)), new ButtonData('3', getFlex(1)), new ButtonData(Operation.subtraction, getFlex(1)), new ButtonData(SpecialSign.pi, getFlex(1)), new ButtonData(SpecialSign.e, getFlex(1))],
     [new ButtonData('0', '2 0 calc(90px)'), new ButtonData(Actions.fraction, getFlex(1)), new ButtonData('+', getFlex(1)), new ButtonData(Actions.compute, '2 0 calc(90px)')],
   ];
--- src/app/calculator/calculalator.ts
+++ src/app/calculator/calculalator.ts
@@ -1,3 +1,4 @@
+import { ReturnStatement } from '@angular/compiler';
 import { BinaryTree } from '../classes/tree/tree_node';
 
 export class Calculator {
@@ -37,11 +38,18 @@ export class Calculator {
             case Actions.compute:
                 console.log("Start Computing");
                 
-                const binaryTree = new BinaryTree();
-                binaryTree.insert(5).insert(4.44).insert("fsf");
+                try{
+                    let expr = new Expression();
+                    expr.createExpression(this.result);
+                    let result = expr.calculateExpression();
+                    console.log(result);
 
-                console.log(this.calculate(this.result));
-                this.result = ResultType.error;
+                    // console.log(this.calculate(this.result));
+                    this.result = `${result}`;
+                } catch (e) {
+                    this.result = ResultType.error;
+                }
+                
                 break;
             default:
                 if ((<any>Object).values(ResultType).includes(this.result)) {
@@ -85,7 +93,7 @@ export enum SpecialSign {
 }
 
 export enum Operation {
-    multiply = "x",
+    multiplication = "x",
     division = "÷",
     leftBrace = "(",
     rightBrace = ")",
@@ -100,15 +108,166 @@ class Expression {
     left: Expression | undefined
     right: Expression | undefined
 
-    calculate = (): number => {
+    // убираем пробелы в строке
+    private skipSpaces = (inString: string): string => {
+        return inString.replace(/ /g, '.');
+    }
+
+    private isNumber(value: string | number): boolean {
+        return ((value != null) && ((value != undefined)) && (value !== '') &&
+           !isNaN(Number(value.toString())));
+    }
+
+    parseFloat = (model: RavCalcExp): number | null => {
+        var remainingStr = this.skipSpaces(model.value);
+        var result: number;
+
+        var numSize: number = 0;
+        if (remainingStr.length > 0 && this.isNumber(remainingStr[0])) {
+            while (numSize < remainingStr.length && this.isNumber(remainingStr[numSize])) {
+                ++numSize;
+            }
+            
+            result = parseFloat(remainingStr.substring(0,numSize));
+            model.value = remainingStr.substring(numSize);
+            return result;
+        }
+        return null;
+    }
+
+    // парсинг оператора
+    parseOperator = (model: RavCalcExp): Operation | null => {
+        var remainingStr = this.skipSpaces(model.value);
+        var op: Operation | null = null;
+        var numSize: number = 0;
+
+        if (remainingStr.length == 0) {
+            return op;
+        } 
+
+        switch (remainingStr[0]) {
+            case Operation.addition:
+                op = Operation.addition; break;
+            case Operation.division:
+                op = Operation.division; break;
+            case Operation.multiplication:
+                op = Operation.multiplication; break;
+            case Operation.subtraction:
+                op = Operation.subtraction; break;
+            default:
+                op = null; break;
+        }
+
+        if (op != null) {
+            model.value = remainingStr.substring(1);
+        }
+        
+        return op;
+    }
+
+    // парсинг сложения-вычитания
+    parseAddSub = (model: RavCalcExp): Expression => {
+        var left = this.parseMulDiv(model); 
+
+        while (true) {
+            var op: Operation | null = Operation.notAnOperation;
+            var remainStr = new RavCalcExp(model.value);
+            op = this.parseOperator(remainStr);
+            if ((op == null) || (op != Operation.addition && op != Operation.subtraction)) {
+                return left;
+            }
+            model.value = remainStr.value;
+
+            var right: Expression | null = null;
+
+            try {
+                right = this.parseMulDiv(model);
+            } catch (e) {
+                this.disposeExpression(left);
+                throw e;
+            }
+
+            try {
+                let expr = new Expression();
+                expr.left = left;
+                expr.right = right
+                expr.op = op;
+                left = expr;
+            } catch (e) {
+                this.disposeExpression(left);
+                this.disposeExpression(right);
+                throw e;
+            }
+        }
+
+        return left;
+    }
+
+    // парсинг умножения-деления
+    parseMulDiv =  (model: RavCalcExp): Expression => {
+        var left = this.parseAtom(model);
+
+        while (true) {
+            var op: Operation | null = Operation.notAnOperation;
+            var remainStr = new RavCalcExp(model.value);
+            op = this.parseOperator(remainStr);
+            if ((op == null) || (op != Operation.division && op != Operation.multiplication)) {
+                return left;
+            }
+            model.value = remainStr.value;
+
+            var right: Expression | null = null;
+            try {
+                right = this.parseAtom(model);
+            } catch (e) {
+                this.disposeExpression(left);
+                throw e;
+            }
+
+            try {
+                let expr = new Expression();
+                expr.left = left;
+                expr.right = right
+                expr.op = op;
+                left = expr;
+            } catch (e) {
+                this.disposeExpression(left);
+                this.disposeExpression(right);
+                throw e;
+            }
+            
+        }
+
+        return left; 
+    }
+
+    // парсинг числа
+    parseAtom = (model:RavCalcExp): Expression => {
+        let expr: Expression = new Expression();
+        let newNumber = this.parseFloat(model);
+
+        if (newNumber == null) {
+            throw new Error();
+        }
+        expr.value = newNumber;
+        return expr;
+    }
+
+
+    disposeExpression = (model: Expression) => {
+        //this.result = ResultType.default;
+    }
+
+    // вычисление выражения 
+    calculateExpression = (): number => {
         if (this.op == Operation.notAnOperation) {
             return this.value;
         }
 
         // assert(this.left);
         // assert(this.right);
-        let leftValue = this.left?.calculate();
-        let rightValue = this.right?.calculate();
+        let leftValue = this.left?.calculateExpression();
+        let rightValue = this.right?.calculateExpression();
         
         switch (this.op) {
             case Operation.addition:
@@ -117,14 +276,33 @@ class Expression {
             case Operation.subtraction:
                 this.value = this.left!.value - this.right!.value;
                 break;
-            case Operation.multiply:
+            case Operation.multiplication:
                 this.value = this.left!.value * this.right!.value;
                 break;
             case Operation.division:
                 this.value = this.left!.value / this.right!.value;
                 break;
         }
-
         return this.value;
-    } 
+    }
+
+    createExpression = (inString: string) => {
+        let calcExpression = new RavCalcExp(inString);
+
+        let ext = this.parseAddSub(calcExpression);
+        this.op = ext.op;
+        this.left = ext.left
+        this.right = ext.right
+        this.value = ext.value;
+        return;
+    }
+}
+
+
+export class RavCalcExp{
+    value: string = "0"
+
+    constructor(expr: string) {
+        this.value = expr;
+    }
 }
