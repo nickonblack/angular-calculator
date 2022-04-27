6caf617520810730fda366b2e034f0634677db92
nikita <nickonblack@gmail.com>
2022-04-27 15:38:26 +0300

    исправления в разметке

--- src/app/utils/help_funcs.ts
+++ src/app/utils/help_funcs.ts
@@ -1,7 +1,7 @@
 // Получаем значение flex по значению
 export function getFlex(flex: number): string{
     if (flex == 1) 
-        return flex + " 1 auto";
+        return `${flex} 1 calc(${45*flex}px)`;
     else 
-        return flex + `1 calc(${45*flex}px)`
+        return `${flex} 1 calc(${45*flex + 10}px)`;
 }
